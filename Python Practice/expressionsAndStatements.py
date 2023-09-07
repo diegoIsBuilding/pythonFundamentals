9a9c97df54119a7282b78e8c2e667af8d7a49a5a
gdiego07 <40152772+gdiego07@users.noreply.github.com>
2023-09-06 17:14:07 -0700

    Expressions, Statements, and Comments
    An is any code that returns a value like 1+1

---
+++ Python Practice/expressionsAndStatements.py
@@ -0,0 +1,10 @@
+#An expression is any code that returns a value
+#For example:
+number_one = 1
+number_two = 1
+addition = number_one + number_two
+print(addition)
+
+name = 'Diego' #This is an inline comment
+print (f"My name is {name}")
+
