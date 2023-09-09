a44e29e808781a1f851ed5a20b642d0da7618069
gdiego07 <40152772+gdiego07@users.noreply.github.com>
2023-09-09 14:05:02 -0700

    Built In Functions and Enums (Constants)

---
+++ Python Practice/enum.py
@@ -0,0 +1,7 @@
+from enum import Enum
+#This data type allouws you to create sets of named constants - considered members of the containing enum
+#Essentially no one can reassign the variable 
+INACTIVE = 0
+ACTIVE = 1
+    
+print(list(State))
