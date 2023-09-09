d7810e99c59896801be174e0755c10980a711546
gdiego07 <40152772+gdiego07@users.noreply.github.com>
2023-09-09 13:25:51 -0700

    Number Data Types
    Complex numbers are written with a 'j' as the imaginary part

---
+++ Python Practice/numberDataTypes.py
@@ -0,0 +1,8 @@
+#The complex() function returns a complex number by specifying a real number and an imaginary number
+#Imaginary numbers are written 'i' in Mathematics - 'j' in engineering
+#In python complex numbers are written with a 'j' as the imaginary part"
+
+num_one = 2 + 3j
+num_two = complex(2, 3)
+
+print(num_two.real, num_two.imag)
